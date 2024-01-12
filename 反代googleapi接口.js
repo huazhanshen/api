d2841ae08ecc8287504970128497d1c636242477
huazhanshen <39611455+huazhanshen@users.noreply.github.com>
2024-01-12 17:34:22 +0800

    Add files via upload

---
+++ 反代googleapi接口.js
@@ -0,0 +1,20 @@
+const TELEGRAPH_URL = 'https://generativelanguage.googleapis.com';
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  const url = new URL(request.url);
+  url.host = TELEGRAPH_URL.replace(/^https?:\/\//, '');
+  const modifiedRequest = new Request(url.toString(), {
+    headers: request.headers,
+    method: request.method,
+    body: request.body,
+    redirect: 'follow'
+  });
+  const response = await fetch(modifiedRequest);
+  const modifiedResponse = new Response(response.body, response);
+  modifiedResponse.headers.set('Access-Control-Allow-Origin', '*');
+  return modifiedResponse;
+}
